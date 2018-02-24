4959f7ada19b158436873e640317310aac3304f5
Craig Taub <craigtaub@gmail.com>
2018-02-24 13:32:44 +0000

    split namespace + listeners

--- newspace.js
+++ newspace.js
@@ -1,25 +1,25 @@
 // load polyfill if native support is unavailable
 if (!process.addAsyncListener) require('async-listener');
+process.addAsyncListener({
+  create: () => namespace.active,
+  before: (context, storage) => { 
+    if (storage) {
+      namespace.active = storage;
+    }
+  },
+  after: (context, storage) => { 
+    if (storage) {
+      namespace.active = {};
+    }
+  }
+});
 
 const namespace = {
   active: {},
   set: (key, val) => {
     namespace.active[key] = val;
   },
-  get: (key) => namespace.active[key],
-  listen: process.addAsyncListener({
-    create: () => namespace.active,
-    before: (context, storage) => { 
-      if (storage) {
-        namespace.active = storage;
-      }
-    },
-    after: (context, storage) => { 
-      if (storage) {
-        namespace.active = {};
-      }
-    }
-  })
+  get: (key) => namespace.active[key]
 };
 
 module.exports = namespace;
